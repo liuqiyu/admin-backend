b263aa132cedadbeeeda507165b08c5f1d76c43d
liuqiyu <1064839051@qq.com>
2019-05-11 17:16:50 +0800

    '控制器及包管理器更新'

--- controller/users/login.js
+++ controller/users/login.js
@@ -1,13 +1,17 @@
-const login = (req, res) => {
-    const data = {
-        username: req.body.username,
-        role: 1,
-    };
-    return res.send({
-        code: 200,
-        message: '',
-        data,
-    });
+import mysql from './../../utils/mysql';
+
+export const login = (req, res, next) => {
+    const sql = 'SELECT * FROM db';
+    mysql.query(sql, (err,result) => {
+        if (err) {
+            console.log('[SELECT ERROR]:', err.message);
+        }
+        res.send({
+            status: 1,
+            count: 2,
+            data: result
+        })
+    })
 };
 
 export default login;
